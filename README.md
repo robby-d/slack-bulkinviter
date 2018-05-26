6ad9e24cc3d8755638212086cb05f7127fb49d34
Timothy Demarest <demarest@gmail.com>
2018-05-25 20:03:25 -0700

    Added command line options for apikey, channel Added support for skipping or adding bots and apps

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Super quick and dirty Python script to bulk invite ALL users in a slack team to 
 To use:
 * Install the [slacker](https://github.com/os/slacker) library via `pip`
 * [Get an API key](https://get.slack.help/hc/en-us/articles/215770388-Creating-and-regenerating-API-tokens)
-* Create a file in the same directory as `slack-bulkinviter.py` called `apikey.txt`, containing your API key
-* Execute the script, passing the name of the channel where all users will be invited, such as `./slack-bulkinviter.py mychannel`
+* Create a file in the same directory as `slack-bulkinviter.py` containing your API key and pass the filename to the argument `--apikey` or `-k`
+* Execute the script, passing the name of the channel where all users will be invited, such as `./slack-bulkinviter.py --channel mychannel`
 * Sit back and let it do its work
--- slack-bulkinviter.py
+++ slack-bulkinviter.py
@@ -1,22 +1,27 @@
 #! /usr/bin/env python3
 import sys
+import argparse
 from slacker import Slacker, Error
 
-# Get channel name from command line
-try:
-    channel_name = sys.argv[1].strip('\'"')
-    assert channel_name
-except:
-    print("Please specify a channel name")
-    sys.exit(1)
+# Initiate the parser
+parser = argparse.ArgumentParser()
+
+# add long and short argument
+parser.add_argument("-b", "--bots", action='store_true', help="Include bots in channel")
+parser.add_argument("-a", "--apps", action='store_true', help="Include apps in channel")
+parser.add_argument("-c", "--channel", required=True, metavar="<Channel Name>", help="Set channel name to add members")
+parser.add_argument("-k", "--apikey", required=True, metavar="<filename>", help="Path to API Key file")
+
+# read arguments from the command line
+args = parser.parse_args()
 
 # Load API key from apikey.txt
 try:
-    with open('apikey.txt') as f:
+    with open(args.apikey) as f:
         api_key = f.read().strip()
         assert api_key
 except IOError:
-    print("Cannot find apikey.txt, or other reading error")
+    print("Cannot find API Key file {}, or other reading error".format(args.apikey))
     sys.exit(1)
 except AssertionError:
     print("Empty API key")
@@ -26,7 +31,7 @@ else:
 
 # Get channel id from name
 response = slack.channels.list()
-channels = [d for d in response.body['channels'] if d['name'] == channel_name]
+channels = [d for d in response.body['channels'] if d['name'] == args.channel]
 if not len(channels):
     print("Cannot find channel")
     sys.exit(1)
@@ -35,19 +40,24 @@ channel_id = channels[0]['id']
 
 # Get users list
 response = slack.users.list()
+
 users = [(u['id'], u['name']) for u in response.body['members']]
 
 # Invite all users to slack channel
 for user_id, user_name in users:
-    print("Inviting {} to {}".format(user_name, channel_name))
+    print("Inviting {} to {}".format(user_name, args.channel))
     try:
         slack.channels.invite(channel_id, user_id)
     except Error as e:
         code = e.args[0]
         if code == "already_in_channel":
             print("{} is already in the channel".format(user_name))
-        elif code in ('cant_invite_self', 'cant_invite', 'user_is_ultra_restricted', 'ura_max_channels', 'cant_invite_app_user', 'is_bot', 'is_app_user'):
+        elif code in ('cant_invite_self', 'cant_invite', 'user_is_ultra_restricted', 'ura_max_channels', 'cant_invite_app_user'):
             print("Skipping user {} ('{}')".format(user_name, code))
+        elif code in ('is_bot') and not args.bots:
+            print("Skipping bot user {} ('{}')".format(user_name, code))
+        elif code in ('is_app_user') and not args.apps:
+            print("Skipping app user {} ('{}')".format(user_name, code))
         else:
             raise
 
